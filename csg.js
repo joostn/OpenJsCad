3b5981ce32f8f9600eaa797d3724602c154330a5
Joost Nieuwenhuijse <joost@newhouse.nl>
2013-01-15 14:37:22 +0100

    Added original extrude() function, renamed tedbeer's to extrudeExperimental

--- csg.js
+++ csg.js
@@ -5185,7 +5185,103 @@ CAG.prototype = {
 		return result;
 	},
 
+  // extruded=cag.extrude({offset: [0,0,10], twistangle: 360, twiststeps: 100});
+  // linear extrusion of 2D shape, with optional twist
+  // The 2d shape is placed in in z=0 plane and extruded into direction <offset> (a CSG.Vector3D)
+  // The final face is rotated <twistangle> degrees. Rotation is done around the origin of the 2d shape (i.e. x=0, y=0)
+  // twiststeps determines the resolution of the twist (should be >= 1)
+  // returns a CSG object
+  extrude: function(options) {
+    if(this.sides.length == 0)
+    {
+      // empty!
+      return new CSG();
+    }
+    var offsetvector = CSG.parseOptionAs3DVector(options, "offset", [0,0,1]);
+    var twistangle = CSG.parseOptionAsFloat(options, "twistangle", 0);
+    var twiststeps = CSG.parseOptionAsInt(options, "twiststeps", 10);
+    
+    if(twistangle == 0) twiststeps = 1;
+    if(twiststeps < 1) twiststeps = 1;
+    
+    var newpolygons = [];
+    var prevtransformedcag;
+    var prevstepz;
+    for(var step=0; step <= twiststeps; step++)
+    {
+      var stepfraction = step / twiststeps;
+      var transformedcag = this;
+      var angle = twistangle * stepfraction;
+      if(angle != 0)
+      {
+        transformedcag = transformedcag.rotateZ(angle);
+      }
+      var translatevector = new CSG.Vector2D(offsetvector.x, offsetvector.y).times(stepfraction);
+      transformedcag = transformedcag.translate(translatevector);
+      var bounds = transformedcag.getBounds();
+      bounds[0] = bounds[0].minus(new CSG.Vector2D(1,1));
+      bounds[1] = bounds[1].plus(new CSG.Vector2D(1,1));
+      var stepz = offsetvector.z * stepfraction;
+      if( (step == 0) || (step == twiststeps) )
+      {
+        // bottom or top face:
+        var csgshell = transformedcag.toCSG(stepz-1, stepz+1);
+        var csgplane = CSG.fromPolygons([new CSG.Polygon([
+          new CSG.Vertex(new CSG.Vector3D(bounds[0].x, bounds[0].y, stepz)),
+          new CSG.Vertex(new CSG.Vector3D(bounds[1].x, bounds[0].y, stepz)),
+          new CSG.Vertex(new CSG.Vector3D(bounds[1].x, bounds[1].y, stepz)),
+          new CSG.Vertex(new CSG.Vector3D(bounds[0].x, bounds[1].y, stepz))
+        ])]);
+        var flip = (step == 0);
+        if(offsetvector.z < 0) flip = !flip;
+        if(flip)
+        {
+          csgplane = csgplane.inverse();
+        }
+        csgplane = csgplane.intersect(csgshell);
+        // only keep the polygons in the z plane:
+        csgplane.polygons.map(function(polygon){
+          if(Math.abs(polygon.plane.normal.z) > 0.99)
+          {
+            newpolygons.push(polygon);
+          }
+        });
+      }
+      if(step > 0)
+      {
+        var numsides = transformedcag.sides.length;
+        for(var sideindex = 0; sideindex < numsides; sideindex++)
+        {
+          var thisside = transformedcag.sides[sideindex];
+          var prevside = prevtransformedcag.sides[sideindex];
+          var p1 = new CSG.Polygon([
+            new CSG.Vertex(thisside.vertex1.pos.toVector3D(stepz)),
+            new CSG.Vertex(thisside.vertex0.pos.toVector3D(stepz)),
+            new CSG.Vertex(prevside.vertex0.pos.toVector3D(prevstepz))
+          ]);
+          var p2 = new CSG.Polygon([
+            new CSG.Vertex(thisside.vertex1.pos.toVector3D(stepz)),
+            new CSG.Vertex(prevside.vertex0.pos.toVector3D(prevstepz)),
+            new CSG.Vertex(prevside.vertex1.pos.toVector3D(prevstepz))
+          ]);
+          if(offsetvector.z < 0)
+          {
+            p1 = p1.flipped();
+            p2 = p2.flipped();
+          }
+          newpolygons.push(p1);
+          newpolygons.push(p2);
+        }
+      }
+      prevtransformedcag = transformedcag;
+      prevstepz = stepz;
+    } // for step
+    return CSG.fromPolygons(newpolygons);
+  },
+
 	/**
+	 * Extrude with custom transformation. Still buggy: may return non-watertight solids.
+	 * 
 	 * extruded=cag.extrude({offset: [0,0,10], twistangle: 360, twiststeps: 100});
 	 * linear extrusion of 2D shape, with optional twist
 	 * The 2d shape is placed in in z=0 plane and extruded into direction <offset> (a CSG.Vector3D)
@@ -5207,7 +5303,7 @@ CAG.prototype = {
 	 * }
 	 *
 	 */
-	extrude: function(options) {
+	extrudeExperimental: function(options) {
 		if(this.sides.length === 0) {
 			// empty!
 			return new CSG();
