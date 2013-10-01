fef1be009346d1cc1b92629296a39ef35d2ced19
Joost Nieuwenhuijse <joost@newhouse.nl>
2013-10-01 15:51:46 +0200

    Speed optimization
    new implementation for CSG.fuzzyFactory

--- csg.js
+++ csg.js
@@ -4479,14 +4479,8 @@ CSG.reTesselateCoplanarPolygons = function(sourcepolygons, destpolygons) {
 //     for example for a 2D rectangle this would be 2
 //   tolerance: The maximum difference for each parameter allowed to be considered a match
 CSG.fuzzyFactory = function(numdimensions, tolerance) {
-	var lookuptable = [];
-	for(var i = 0; i < numdimensions; i++) {
-		lookuptable.push({});
-	}
-	this.lookuptable = lookuptable;
-	this.nextElementId = 1;
+	this.lookuptable = {};
 	this.multiplier = 1.0 / tolerance;
-	this.objectTable = {};
 };
 
 CSG.fuzzyFactory.prototype = {
@@ -4496,100 +4490,40 @@ CSG.fuzzyFactory.prototype = {
 	// If not found, calls the supplied callback function which should create a new object with
 	// the specified properties. This object is inserted in the lookup database.
 	lookupOrCreate: function(els, creatorCallback) {
-		var object;
-		var key = this.lookupKey(els);
-		if(key === null) {
-			object = creatorCallback(els);
-			key = this.nextElementId++;
-			this.objectTable[key] = object;
-			for(var dimension = 0; dimension < els.length; dimension++) {
-				var elementLookupTable = this.lookuptable[dimension];
-				var value = els[dimension];
-				var valueMultiplied = value * this.multiplier;
-				var valueQuantized1 = Math.floor(valueMultiplied);
-				var valueQuantized2 = Math.ceil(valueMultiplied);
-				CSG.fuzzyFactory.insertKey(key, elementLookupTable, valueQuantized1);
-				CSG.fuzzyFactory.insertKey(key, elementLookupTable, valueQuantized2);
+		var hash = "";
+		var multiplier = this.multiplier;
+		els.forEach(function(el){
+			var valueQuantized = Math.round(el * multiplier);
+			hash += valueQuantized + "/";
+		});
+		if(hash in this.lookuptable)
+		{
+			return this.lookuptable[hash];
+		}
+		else
+		{
+			var object = creatorCallback(els);
+			var hashparts = els.map(function(el) {
+				var q0 = Math.floor(el * multiplier);
+				var q1 = q0 + 1;
+				return [""+q0+"/", ""+q1+"/"];
+			});
+			var numelements = els.length;
+			var numhashes = 1 << numelements;
+			for(var hashmask = 0; hashmask < numhashes; ++hashmask) {
+				var hashmask_shifted = hashmask;
+				hash = "";
+				hashparts.forEach(function(hashpart){
+					hash += hashpart[hashmask_shifted & 1];
+					hashmask_shifted >>= 1;
+				});
+				this.lookuptable[hash] = object;
 			}
-		} else {
-			object = this.objectTable[key];
+			return object;
 		}
-		return object;
 	},
-
-	// ----------- PRIVATE METHODS:
-	lookupKey: function(els) {
-		var keyset = {};
-		for(var dimension = 0; dimension < els.length; dimension++) {
-			var elementLookupTable = this.lookuptable[dimension];
-			var value = els[dimension];
-			var valueQuantized = Math.round(value * this.multiplier);
-			valueQuantized += "";
-			if(valueQuantized in elementLookupTable) {
-				if(dimension === 0) {
-					keyset = elementLookupTable[valueQuantized];
-				} else {
-					keyset = CSG.fuzzyFactory.intersectSets(keyset, elementLookupTable[valueQuantized]);
-				}
-			} else {
-				return null;
-			}
-			if(CSG.fuzzyFactory.isEmptySet(keyset)) return null;
-		}
-		// return first matching key:
-		for(var key in keyset) return key;
-		return null;
-	},
-
-	lookupKeySetForDimension: function(dimension, value) {
-		var result;
-		var elementLookupTable = this.lookuptable[dimension];
-		var valueMultiplied = value * this.multiplier;
-		var valueQuantized = Math.floor(value * this.multiplier);
-		if(valueQuantized in elementLookupTable) {
-			result = elementLookupTable[valueQuantized];
-		} else {
-			result = {};
-		}
-		return result;
-	}
 };
 
-CSG.fuzzyFactory.insertKey = function(key, lookuptable, quantizedvalue) {
-	if(quantizedvalue in lookuptable) {
-		lookuptable[quantizedvalue][key] = true;
-	} else {
-		var newset = {};
-		newset[key] = true;
-		lookuptable[quantizedvalue] = newset;
-	}
-};
-
-CSG.fuzzyFactory.isEmptySet = function(obj) {
-	for(var key in obj) return false;
-	return true;
-};
-
-CSG.fuzzyFactory.intersectSets = function(set1, set2) {
-	var result = {};
-	for(var key in set1) {
-		if(key in set2) {
-			result[key] = true;
-		}
-	}
-	return result;
-};
-
-CSG.fuzzyFactory.joinSets = function(set1, set2) {
-	var result = {}, key;
-	for(key in set1) {
-		result[key] = true;
-	}
-	for(key in set2) {
-		result[key] = true;
-	}
-	return result;
-};
 
 //////////////////////////////////////
 CSG.fuzzyCSGFactory = function() {
