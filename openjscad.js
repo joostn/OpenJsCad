joostn/OpenJsCad

commit fa01879a083b28d2ae5be8723cbb2ecffafe0aa1

Replaced BlobBuilder by new Blob() constructor
BlobBuilder is deprecated and already seems to have been removed from
the latest Chrome version

openjscad.js
@@ -373,15 +373,6 @@ OpenJsCad.parseJsCadScriptASync = function(script, mainParameters, callback) {
   return worker;
 };
 
-OpenJsCad.getBlobBuilder = function() {
-  var bb;
-  if(window.BlobBuilder) bb = new window.BlobBuilder()
-  else if(window.WebKitBlobBuilder) bb = new window.WebKitBlobBuilder()
-  else if(window.MozBlobBuilder) bb = new window.MozBlobBuilder()
-  else throw new Error("Your browser doesn't support BlobBuilder");
-  return bb;
-};
-
 OpenJsCad.getWindowURL = function() {
   if(window.URL) return window.URL;
   else if(window.webkitURL) return window.webkitURL;
@@ -389,11 +380,8 @@ OpenJsCad.getWindowURL = function() {
 };
 
 OpenJsCad.textToBlobUrl = function(txt) {
-  var bb=OpenJsCad.getBlobBuilder();
   var windowURL=OpenJsCad.getWindowURL();
-
-  bb.append(txt);
-  var blob = bb.getBlob();
+  var blob = new Blob([txt]);
   var blobURL = windowURL.createObjectURL(blob)
   if(!blobURL) throw new Error("createObjectURL() failed"); 
   return blobURL;
@@ -845,26 +833,24 @@ OpenJsCad.Processor.prototype = {
   },
 
   currentObjectToBlob: function() {
-    var bb=OpenJsCad.getBlobBuilder();
-    var mimetype = this.selectedFormatInfo().mimetype;
     var format = this.selectedFormat();
     
+    var blob;
     if(format == "stl")
     {      
-      this.currentObject.fixTJunctions().toStlBinary(bb);
+      blob=this.currentObject.fixTJunctions().toStlBinary();
     }
     else if(format == "x3d") {
-      this.currentObject.fixTJunctions().toX3D(bb);
+      blob=this.currentObject.fixTJunctions().toX3D(bb);
     }
     else if(format == "dxf")
     {
-      this.currentObject.toDxf(bb);
+      blob=this.currentObject.toDxf();
     }
     else
     {
       throw new Error("Not supported");
     }    
-    var blob = bb.getBlob(mimetype);
     return blob;
   },
   

csg.js
@@ -416,7 +416,7 @@ CSG.prototype = {
     return result;
   },
 
-  toX3D: function(blobbuilder) {
+  toX3D: function() {
     // materialPolygonLists
     // key: a color string (e.g. "0 1 1" for yellow)
     // value: an array of strings specifying polygons of this color
@@ -518,11 +518,12 @@ CSG.prototype = {
       }
     }
     
-    blobbuilder.append((new XMLSerializer()).serializeToString(exportDoc));
+    var x3dstring=(new XMLSerializer()).serializeToString(exportDoc);
+    return new Blob([x3dstring], {type: "model/x3d+xml"});
   },
   
   // see http://en.wikipedia.org/wiki/STL_%28file_format%29#Binary_STL
-  toStlBinary: function(blobbuilder) {
+  toStlBinary: function() {
     // first check if the host is little-endian:
     var buffer = new ArrayBuffer(4);
     var int32buffer = new Int32Array(buffer, 0, 1);
@@ -544,35 +545,45 @@ CSG.prototype = {
     {
       headerarray[i]=65;
     }
-    blobbuilder.append(headerarray.buffer);
     var ar1 = new Uint32Array(1);
     ar1[0] = numtriangles;
-    blobbuilder.append(ar1.buffer);
-    buffer = new ArrayBuffer(50);
+    // write the triangles to allTrianglesBuffer:
+    var allTrianglesBuffer = new ArrayBuffer(50*numtriangles);
+    var allTrianglesBufferAsInt8=new Int8Array(allTrianglesBuffer);
+    // a tricky problem is that a Float32Array must be aligned at 4-byte boundaries (at least in certain browsers)
+    // while each triangle takes 50 bytes. Therefore we write each triangle to a temporary buffer, and copy that
+    // into allTrianglesBuffer:
+    var triangleBuffer = new ArrayBuffer(50);
+    var triangleBufferAsInt8=new Int8Array(triangleBuffer);
+    // each triangle consists of 12 floats:
+    var triangleFloat32array = new Float32Array(triangleBuffer, 0, 12);
+    // and one uint16:
+    var triangleUint16array = new Uint16Array(triangleBuffer, 48, 1);
     var byteoffset=0;
     this.polygons.map(function(p){ 
       var numvertices = p.vertices.length;
       for(var i = 0; i < numvertices-2; i++)
       {
-        var float32array = new Float32Array(buffer, 0, 12);
         var normal = p.plane.normal;
-        float32array[0] = normal._x;
-        float32array[1] = normal._y;
-        float32array[2] = normal._z;
+        triangleFloat32array[0] = normal._x;
+        triangleFloat32array[1] = normal._y;
+        triangleFloat32array[2] = normal._z;
         var arindex = 3;
         for(var v = 0; v < 3; v++)
         {
           var vv=v + ((v > 0)? i:0);
           var vertexpos = p.vertices[vv].pos;
-          float32array[arindex++] = vertexpos._x;
-          float32array[arindex++] = vertexpos._y;
-          float32array[arindex++] = vertexpos._z;
+          triangleFloat32array[arindex++] = vertexpos._x;
+          triangleFloat32array[arindex++] = vertexpos._y;
+          triangleFloat32array[arindex++] = vertexpos._z;
         }
-        var uint16array = new Uint16Array(buffer, 48, 1);
-        uint16array[0]=0;
-        blobbuilder.append(buffer);
+        triangleUint16array[0]=0;
+        // copy the triangle into allTrianglesBuffer:
+        allTrianglesBufferAsInt8.set(triangleBufferAsInt8, byteoffset);
+        byteoffset += 50;
       }
     });
+    return new Blob([headerarray.buffer, ar1.buffer, allTrianglesBuffer], {type: "application/sla"});
   },
   
   toString: function() {
@@ -5885,19 +5896,18 @@ CAG.prototype = {
     return paths;
   },
   
-  toDxf: function(blobbuilder) {
+  toDxf: function() {
     var paths = this.getOutlinePaths();
-    return CAG.PathsToDxf(paths, blobbuilder);
+    return CAG.PathsToDxf(paths);
   },
     
 };
 
-CAG.PathsToDxf = function(paths, blobbuilder) {
+CAG.PathsToDxf = function(paths) {
   var str = "999\nDXF generated by OpenJsCad\n  0\nSECTION\n  2\nENTITIES\n";
-  blobbuilder.append(str);
   paths.map(function(path) {
     var numpoints_closed = path.points.length + (path.closed? 1:0);
-    str = "  0\nLWPOLYLINE\n  90\n"+numpoints_closed+"\n  70\n"+(path.closed? 1:0)+"\n";
+    str += "  0\nLWPOLYLINE\n  90\n"+numpoints_closed+"\n  70\n"+(path.closed? 1:0)+"\n";
     for(var pointindex = 0; pointindex < numpoints_closed; pointindex++)
     {
       var pointindexwrapped = pointindex;
@@ -5905,10 +5915,9 @@ CAG.PathsToDxf = function(paths, blobbuilder) {
       var point = path.points[pointindexwrapped];
       str += " 10\n"+point.x+"\n 20\n"+point.y+"\n 30\n0.0\n";
     } 
-    blobbuilder.append(str);
   });
-  str = "  0\nENDSEC\n  0\nEOF\n";
-  blobbuilder.append(str);
+  str += "  0\nENDSEC\n  0\nEOF\n";
+  return new Blob([str], {type: "application/dxf"});
 };
 
 CAG.Vertex = function(pos) {

commit 2538c0e84322eee1fe8e0cc90d4b38747f0bbba8

Fix X3D export
This was broken in fa01879 (Replaced BlobBuilder by new Blob()
constructor) where the BlobBuilder was removed, but the `bb` variable
was still being passed to the `toX3D()` function, resulting in a
ReferenceError.

openjscad.js
@@ -1204,7 +1204,7 @@ OpenJsCad.Processor.prototype = {
       blob=this.currentObject.fixTJunctions().toStlBinary();
     }
     else if(format == "x3d") {
-      blob=this.currentObject.fixTJunctions().toX3D(bb);
+      blob=this.currentObject.fixTJunctions().toX3D();
     }
     else if(format == "dxf")
     {